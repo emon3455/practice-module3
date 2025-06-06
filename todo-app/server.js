0848625636fb77c5c29de05094f5e0ef81707ba3
emon3455 <emon.mhk69@gmail.com>
2025-06-06 12:03:53 +0600

    routing setup done

--- todo-app/server.js
+++ todo-app/server.js
@@ -1,7 +1,18 @@
 const http = require("http");
 
 const server = http.createServer((req,res)=>{
-    res.end("Welcome to TODO-APP server")
+    console.log(req.url);
+    console.log(req.method);
+    
+    if(req.url==="/todo" && req.method==="GET"){
+        res.end("All Todod")
+    }
+    else if(req.url==="/todo/create" && req.method==="POST"){
+        res.end("Create Todo")
+    }else{
+        res.end("Route Not Found")
+    }
+
 })
 
 server.listen(5000, "127.0.0.1",()=>{

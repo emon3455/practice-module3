cdcb80c97b618da081db2d621e988219c1095fb8
emon3455 <emon.mhk69@gmail.com>
2025-06-10 11:33:38 +0600

    issues resolve for search by query

--- todo-app/server.js
+++ todo-app/server.js
@@ -1,21 +1,44 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const url = require("url");
 
 const filePath = path.join(__dirname, "./db/todo.json");
 
 const server = http.createServer((req, res) => {
 
+    const parsedURL = url.parse(req.url);
+    const {pathname, query} = parsedURL;
+
     // get all todo
-    if (req.url === "/todo" && req.method === "GET") {
+    if (pathname === "/todo" && req.method === "GET" && !query) {
         const data = fs.readFileSync(filePath, { encoding: "utf-8" })
         res.writeHead(200, {
             "content-type": "application/json"
         })
         res.end(data)
     }
+    // get single todo by id
+    else if(pathname.startsWith("/todo/")& req.method === "GET"){
+        const id = pathname.split("=")[1];
+        const allTodo = JSON.parse(fs.readFileSync(filePath,{encoding:"utf-8"}));
+        const result = allTodo.find((todo)=>todo.id==id);
+        res.writeHead(200, {
+            "content-type": "application/json"
+        })
+        res.end(JSON.stringify(result));
+    }
+    // get todo by query
+    else if(pathname.startsWith("/todo") && req.method === "GET" && query){
+        const key = query.split("=")[0];
+        const value = query.split("=")[1];
+        
+        const allTodo = JSON.parse(fs.readFileSync(filePath,{encoding:"utf-8"}));
+        const result = allTodo.filter((todo) => String(todo[key])?.includes(String(value)));
+        res.end(JSON.stringify(result));
+    }
     // post a todo
-    else if (req.url === "/todo/create" && req.method === "POST") {
+    else if (pathname === "/todo/create" && req.method === "POST") {
         let data = "";
         req.on("data", (chunk) => {
             data += chunk;
